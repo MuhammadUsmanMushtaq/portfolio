184c0079b096118fd65f4047c5b422c4c831b334
MuhammadUsmanMushtaq <usman820901@gmail.com>
2020-06-26 18:09:54 +0200

    Added responsive design

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -2,6 +2,8 @@ import React from "react";
 import {
   WorkWrapper,
   Project,
+  Content,
+  Tools,
   Description,
   ProjectImg,
   Button,
@@ -12,8 +14,13 @@ function Work() {
   return (
     <WorkWrapper>
       <Project>
-        <Description>
+        <Content>
           <ProjectTitle>KYH Course Landing Page Redesign</ProjectTitle>
+          <Description>
+            School project to redesign the landing page of Front-end program at
+            KYH.
+          </Description>
+          <Tools>HTML5, CSS, Vanilla JavaScript</Tools>
           <Button
             href="https://github.com/MuhammadUsmanMushtaq/frontend_kyh_landing_page"
             target="_blank"
@@ -26,14 +33,18 @@ function Work() {
           >
             Link to site
           </Button>
-        </Description>
+        </Content>
         <ProjectImg>
           <img src="images/kyh_page.png" alt="Image" />
         </ProjectImg>
       </Project>
       <Project>
-        <Description>
+        <Content>
           <ProjectTitle>Easyweb by Sphinxly</ProjectTitle>
+          <Description>
+            A school team project for Easyweb by Sphinxly.
+          </Description>
+          <Tools>Next.js, styled components</Tools>
           <Button
             href="https://github.com/NataliaWiman/fe19tp3_easyweb"
             target="_blank"
@@ -43,60 +54,77 @@ function Work() {
           <Button href="https://fe19tp3-easyweb.now.sh/" target="_blank">
             Link to app
           </Button>
-        </Description>
+        </Content>
         <ProjectImg>
           <img src="images/tp_easyweb.png" alt="Image" />
         </ProjectImg>
       </Project>
       <Project>
-        <Description>
+        <Content>
           <ProjectTitle>Weather App</ProjectTitle>
+          <Description>A simple weather application.</Description>
+          <Tools>HTML5, CSS, Vanilla JavaScript</Tools>
           <Button
             href="https://github.com/MuhammadUsmanMushtaq/weatherapp"
             target="_blank"
           >
             Link to GitHub
           </Button>
-        </Description>
+        </Content>
         <ProjectImg>
           <img src="images/weather_app.png" alt="Image" />
         </ProjectImg>
       </Project>
       <Project>
-        <Description>
-          <ProjectTitle>Nutrition Fact Checker</ProjectTitle>
+        <Content>
+          <ProjectTitle>B-E-V Nutrition tracker</ProjectTitle>
+          <Description>
+            School team project to create a nutrition tracker application using
+            data from an external API.
+          </Description>
+          <Tools>React, styled componenets, chart.js </Tools>
           <Button href="https://github.com/AD757/fe19tp2_lemon" target="_blank">
             Link to GitHub
           </Button>
-        </Description>
+        </Content>
         <ProjectImg>
           <img src="images/tp_nutrition.png" alt="Image" />
         </ProjectImg>
       </Project>
       <Project>
-        <Description>
+        <Content>
           <ProjectTitle>Quire Note Taking App</ProjectTitle>
+          <Description>
+            School team project to create a note taking application.
+          </Description>
+          <Tools>HTML5, CSS, Vanilla JavaScript </Tools>
           <Button
             href="https://github.com/AD757/fe19tp1_TeamStaple"
             target="_blank"
           >
             Link to GitHub
           </Button>
-        </Description>
+        </Content>
         <ProjectImg>
           <img src="images/tp_note.png" alt="Image" />
         </ProjectImg>
       </Project>
       <Project>
-        <Description>
+        <Content>
           <ProjectTitle>UX Review for EU-Bikes</ProjectTitle>
+          <Description>
+            The main aim of this project is to analyze the user experience of
+            the EU bike service and to recommend improvements to satisfy both
+            the user goals and the business goals.
+          </Description>
+          <Tools>Figma, Photoshop </Tools>
           <Button
             href="https://www.figma.com/proto/k60XBI2Tr0mefJXT2Q60jw/EU-BIKE?node-id=1%3A2&viewport=444%2C412%2C0.5&scaling=scale-down"
             target="_blank"
           >
             Link to prototype
           </Button>
-        </Description>
+        </Content>
         <ProjectImg>
           <img src="images/EuBike.png" alt="Image" />
         </ProjectImg>
--- src/components/Work/WorkStyled.js
+++ src/components/Work/WorkStyled.js
@@ -4,16 +4,22 @@ export const WorkWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
-  padding: 60px 20px;
+  padding: 16px 0;
 `;
 
 export const Project = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin: 20px;
-  padding: 24px;
+  margin: 20px 0;
+  padding: 24px 0;
   &:hover img {
     transform: scale(1.04, 1.04);
+  }
+  &:hover a {
+    color: #008bf8;
+  }
+  @media screen and (max-width: 800px) {
+    padding: 12px 0;
   }
 `;
 
@@ -24,20 +30,23 @@ export const Button = styled.a`
   font-size: 12px;
   color: #000;
   text-decoration: none;
-  &:hover {
-    color: #008bf8;
-  }
+  transition: all 0.25s ease-in-out;
 `;
 
-export const Description = styled.div`
+export const Content = styled.div`
   font-size: 18px;
   padding-bottom: 16px;
   width: 260px;
   margin-right: 100px;
+
+  @media screen and (max-width: 800px) {
+    width: 100%;
+    margin: 0;
+  }
 `;
 export const ProjectTitle = styled.h3`
   margin: 0;
-  margin-bottom: 16px;
+  margin-bottom: 8px;
   padding: 0;
 `;
 
@@ -45,10 +54,28 @@ export const ProjectImg = styled.div`
   width: 500px;
   height: 400px;
   background: #edf5fc;
+  border-radius: 6px;
   padding: 48px 120px;
   & img {
     width: 100%;
     height: 100%;
+    border-radius: 6px;
     transition: all 0.25s ease-in-out;
   }
+  @media screen and (max-width: 800px) {
+    width: 100%;
+    height: 300px;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export const Description = styled.p`
+  font-size: 14px;
+  margin: 0;
+`;
+
+export const Tools = styled.p`
+  font-size: 12px;
+  font-style: italic;
 `;
